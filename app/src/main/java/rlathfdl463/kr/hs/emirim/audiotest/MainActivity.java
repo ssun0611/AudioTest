1e297d5525fd3aff62e963b9dfd23236dfdbf1a7
ssun0611 <rlathfdl463@naver.com>
2017-06-19 19:18:39 +0900

    #3 Event Programing (Switch)

--- app/src/main/java/rlathfdl463/kr/hs/emirim/audiotest/MainActivity.java
+++ app/src/main/java/rlathfdl463/kr/hs/emirim/audiotest/MainActivity.java
@@ -1,13 +1,36 @@
 package rlathfdl463.kr.hs.emirim.audiotest;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+    Switch switchmusic;
+    MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switchmusic=(Switch)findViewById(R.id.switch_music);
+        player=MediaPlayer.create(this, R.raw.mr_mouse);
+        switchmusic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(switchmusic.isChecked()){
+                    player.start();
+                }
+                else player.pause();
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        player.stop();
     }
 }
